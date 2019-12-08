adda8a56c259f608b8c3cf14c371815567cc3d6d
Luke Sherwood <luke.sherwood55@gmail.com>
2019-12-08 07:53:21 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++
@@ -1,9 +0,0 @@
-def display_board (input=[" "," "," "," "," "," "," "," "," "])
-  puts display_board
-end
-
-  #puts  " #input[0] | #input[1] | #input[2] "
-  #puts  -----------
-  #puts  " #input[3] | #input[4] | #input[5] "
-  #puts  ---------
-  #puts  " #input[6] | #input[7] | #input[8] "
